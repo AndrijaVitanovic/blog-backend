0791998afb136c7ae44715514e17a3ca363f5f21
David Stefanovic <david.stefanovic0711@gmail.com>
2022-12-19 18:49:29 +0100

    added RegisterUserDto

---
+++ src/main/java/com/blog/data/RegisterUserDto.java
@@ -0,0 +1,13 @@
+package com.blog.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterUserDto {
+}
