25638631980c8077baf75aa3b336fb739e2d0dc7
David Stefanovic <david.stefanovic0711@gmail.com>
2022-12-20 16:11:47 +0100

    added comments

--- src/main/java/com/blog/service/TemplateProcessor.java
+++ src/main/java/com/blog/service/TemplateProcessor.java
@@ -4,7 +4,20 @@ import java.util.Map;
 
 public interface TemplateProcessor {
 
+    /**
+     * Renders the given template with the given model.
+     *
+     * @param template name of the template
+     * @param model model data to be rendered
+     * @return rendered template
+     */
     String process(String template, Map<String, Object> model);
 
+    /**
+     * Renders the given template.
+     *
+     * @param template name of the template
+     * @return rendered template
+     */
     String process(String template);
 }
