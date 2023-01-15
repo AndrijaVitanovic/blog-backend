a300c5e36dd394afb7845d601379fda9d0d28684
David Stefanovic <david.stefanovic0711@gmail.com>
2023-01-15 02:18:03 +0100

    changed deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-mvn clean package -DskipTests &&
-  target/blog-0.0.1-SNAPSHOT.war root@138.68.79.111:/opt/tomcat/webapps/blog.war
+mvn clean package -DskipTests && \
+  scp -r target/blog-0.0.1-SNAPSHOT.war root@138.68.79.111:/opt/tomcat/webapps/blog.war
